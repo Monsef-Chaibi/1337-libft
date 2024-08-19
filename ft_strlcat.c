2ce6ba38fa48b665d311fa646bc3ff8038f3b383
Monsef-Chaibi <xr.monsef@gmail.com>
2024-08-19 11:11:07 +0100

    Add ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+size_t ft_strlen(const char *s)
+{
+    size_t len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+size_t ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t src_len;
+	size_t dest_len;
+	size_t i;
+	size_t x;
+
+	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dest);
+	
+	if (size <= dest_len)
+	{
+		return (src_len + size);
+	}
+	i = dest_len;
+	x = 0;
+	while (src[x] !=  '\0' && i < size - 1)
+	{
+		dest[i] = src[x];
+		i++;
+		x++;
+	}
+	dest[i] = '\0';
+	return (src_len + dest_len);
+}
